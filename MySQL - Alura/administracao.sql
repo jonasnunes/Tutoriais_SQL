be9daefbe251b9d39114b1e206dcafc66bb769ff
Jonas Nunes <70301882+jonasnunes@users.noreply.github.com>
2023-03-11 23:55:09 -0300

    Update administracao.sql

---
+++ MySQL - Alura/administracao.sql
@@ -0,0 +1,5 @@
+-- visualizar as tabelas temporárias no disco
+SHOW GLOBAL STATUS LIKE 'CREATED_TMP_TABLES_DISK';
+
+-- visualizar as tabelas temporárias em memória
+SHOW GLOBAL STATUS LIKE 'CREATED_TMP_TABLES';
