84af1d135a3c2df0397c5f1d6f25eac9b8e5f124
Jonas Nunes <70301882+jonasnunes@users.noreply.github.com>
2023-03-13 11:14:06 -0300

    Update administracao.sql

--- MySQL - Alura/administracao.sql
+++ MySQL - Alura/administracao.sql
@@ -21,3 +21,46 @@ CREATE TABLE TB_DEFAULT (
 	ID INT,
     NOME VARCHAR(100)
 ) ENGINE = MEMORY;
+
+-- mostrando as variáveis de ambiente que possuem DIR em seu nome
+SHOW VARIABLES WHERE VARIABLE_NAME LIKE '%dir';
+
+/*
+	- Para alterar o diretório em que as bases de dados são salvas fisicamente por padrão,
+    altere o caminho na variável de ambiente datadir que se encontra no arquivo my.ini
+    - Pare o serviço do MySQL, reinice e depois feche o Workbencch e abra novamente para que as alterações tenham efeito
+    - Também é necessário copiar a pasta Data do caminho original para novo caminho especificado
+*/
+
+/*
+	- Realizando backup através de linha de comando no CMD do Windows
+    - Primeiro navegue até a pasta onde está localizado o mysqldump
+    - C:\Progam Files\MySQL\MySQL Server 8.0\bin (o diretório pode ser diferente em outra máquina)
+    - C:\MySQL\UniServerZ\core\mysql\bin (outra opção de diretório)
+    
+    mysqldump -uroot -p --databases sucos_vendas > c:\mysqladmin\sucos_vendas_full.sql
+    
+    - Fazendo backup de apenas uma tabela da base de dados
+    
+    mysqldump -uroot -p --databases sucos_vendas --tables notas_fiscais > c:\mysqladmin\backup_notas_fiscais.sql
+    
+    - Fazendo backup de todas as tabelas mas excluindo uma em específico
+    
+    mysqldump -uroot -p --databases sucos_vendas --ignore-table sucos_vendas.notas_fiscais > c:\mysqladmin\ignore_notas_fiscais
+    
+    - Salvando os dados ignorando a estrutura da tabela
+    
+    mysqldump -uroot -p --databases sucos_vendas --no-create-db --no-create-info --complete-insert > c:\mysqladmin\apenas_dados.sql
+*/
+
+-- desligando o banco temporariamente para realizar um backup
+LOCK INSTANCE FOR BACKUP;
+
+-- liberando o banco
+UNLOCK INSTANCE;
+
+/*
+	- Restaurando o banco de dados através da linha de comando
+    
+    mysql -uroot -p < c:\mysqladmin\sucos_vendas_full.sql
+*/
